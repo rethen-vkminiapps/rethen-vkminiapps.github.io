d72bdfdb979c68fe5f8aa8ca401d07db11760290
rethen-vkminiapps <56508589+rethen-vkminiapps@users.noreply.github.com>
2019-10-14 19:41:09 +0600

    Add files via upload

---
+++ precache-manifest.241b0c4eee107626b01f632d9e1f434e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fd8449340b562c7273c",
+    "url": "/static/css/main.688d495f.chunk.css"
+  },
+  {
+    "revision": "7fd8449340b562c7273c",
+    "url": "/static/js/main.260f9dde.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "70aca8d317821178dfa1",
+    "url": "/static/css/2.000ac4a4.chunk.css"
+  },
+  {
+    "revision": "70aca8d317821178dfa1",
+    "url": "/static/js/2.0a07ff3b.chunk.js"
+  },
+  {
+    "revision": "95dbe5502e3150e6db61d803c32bdf30",
+    "url": "/index.html"
+  }
+];
